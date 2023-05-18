632ca4b083e47f0a1ab5e2aa81d35c2e87c98a76
Andru-1987 <andru.ocatorres@gmail.com>
2023-05-18 07:28:13 -0300

    update: bk status strategy

---
+++ beer_company_ALTER.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS beer_company;
+
+CREATE DATABASE beer_company ; 
+
+USE beer_company;
+
+CREATE TABLE beer_category(
+    id_category INT NOT NULL AUTO_INCREMENT,
+    beer_category VARCHAR(255),
+    beer_status_category TINYINT(2) DEFAULT 1,
+    beer_selection VARCHAR(100),
+    PRIMARY KEY (id_category)
+);
+
+
+CREATE TABLE beer(
+    id_beer INT NOT NULL AUTO_INCREMENT,
+    beer_name VARCHAR(255),
+    beer_abv DECIMAL(6,2),
+    beer_url VARCHAR(255),
+    beer_type ENUM('raiz','cebada','trigo','malta') DEFAULT 'malta',
+    beer_price_currency ENUM('USD','PESOS ARGENTINOS','SOLES') DEFAULT 'USD',
+    beer_price DECIMAL(10,2) DEFAULT 10.3,
+    id_category INT,
+    PRIMARY KEY (id_beer)
+);
+
+
+ALTER TABLE beer 
+ADD CONSTRAINT fk_category_beer
+FOREIGN KEY (id_category) REFERENCES beer_category(id_category);
+
+INSERT INTO beer_category values(
+ NULL , 'premium',1,'stout'
+);
+
+INSERT INTO beer  values
+(
+    NULL, 'Stella Artois','12.4', "./some/path", DEFAULT,DEFAULT,DEFAULT,1
+
+);
--- readme.md
+++ readme.md
@@ -2,4 +2,25 @@
 
 En mi caso debo tener mysql para linux, o en la terminal
 
+USANDO LA TERMINAL
+
+```
 mysql --host 127.0.0.1 -P 3306 -u root -p
+```
+
+Entender lo que es una base de datos entendiendo una DER
+
+DER ==> Diagrama de entiedad relacion
+
+BACK UP => GENERAR UN BACK UP CON MYSQL DUMP
+
+```
+mysqldump --host 127.0.0.1 -P 3306 -u root -p  beer_company > bk_beer.sql
+```
+
+RESTORE =>
+
+```
+mysql --host 127.0.0.1 -P 3306 -u root -p beer_restore_bk < bk_beer.sql
+
+```
